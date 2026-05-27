74f62b6fc5ec13a1f62df0f9adfb70d22b07318b
云樱梦海 <qianchilang@example.com>
2026-05-27 12:25:49 +0800

    chore: 添加启动脚本+便携Python+gitignore

--- 启动网站.bat
+++ 启动网站.bat
@@ -1,5 +1,9 @@
 @echo off
 cd /d "%~dp0site"
 echo Starting HTTP server on http://localhost:8080 ...
-start http://localhost:8080/lesson.html
-python -m http.server 8080
+start http://localhost:8080
+if exist "%~dp0python\python.exe" (
+    "%~dp0python\python.exe" -m http.server 8080
+) else (
+    python -m http.server 8080
+)
